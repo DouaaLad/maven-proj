74804364175e638d5ba83df42c60b69b4d60045d
DouaaLad <ladghamdouaa@gmail.com>
2024-05-04 09:19:12 +0000

    first commit

---
+++ src/main/java/bmt/ChromeDriver.java
@@ -0,0 +1,99 @@
+package bmt;
+
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ChromeDriver implements WebDriver {
+
+    public void get(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    public Object findElement(Object xpath) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findElement'");
+    }
+
+    public String getWindowHand() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getWindowHand'");
+    }
+
+    @Override
+    public String getCurrentUrl() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCurrentUrl'");
+    }
+
+    @Override
+    public String getTitle() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTitle'");
+    }
+
+    @Override
+    public List<WebElement> findElements(By by) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findElements'");
+    }
+
+    @Override
+    public WebElement findElement(By by) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findElement'");
+    }
+
+    @Override
+    public String getPageSource() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPageSource'");
+    }
+
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'close'");
+    }
+
+    @Override
+    public void quit() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'quit'");
+    }
+
+    @Override
+    public Set<String> getWindowHandles() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getWindowHandles'");
+    }
+
+    @Override
+    public String getWindowHandle() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getWindowHandle'");
+    }
+
+    @Override
+    public TargetLocator switchTo() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'switchTo'");
+    }
+
+    @Override
+    public Navigation navigate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'navigate'");
+    }
+
+    @Override
+    public Options manage() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'manage'");
+    }
+
+}
